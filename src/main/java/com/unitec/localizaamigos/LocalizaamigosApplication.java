4eaa4e5905c439614c74bca27610fbb9928b5b76
daton <rapidclimate@gmail.com>
2021-02-26 19:20:58 -0600

    Initial commit

---
+++ src/main/java/com/unitec/localizaamigos/LocalizaamigosApplication.java
@@ -0,0 +1,13 @@
+package com.unitec.localizaamigos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalizaamigosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalizaamigosApplication.class, args);
+	}
+
+}
