55c1cbb56593f6f34f48897d10266997e128d646
daton <rapidclimate@gmail.com>
2021-03-05 19:19:12 -0600

    Subiremos a github

---
+++ src/main/java/com/unitec/localizaamigos/ControladorMensaje.java
@@ -0,0 +1,24 @@
+package com.unitec.localizaamigos;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mensaje")
+public class ControladorMensaje {
+
+@GetMapping("/")
+ public  Mensaje obtenerMensaje(){
+ //Creamos un objeto de tipo mensaje
+ Mensaje mensaje=new Mensaje();
+ mensaje.setTitulo("Para mis alumnos de ANDROID");
+ mensaje.setCuerpo("Este es un servicio REST muy basico pero funciona ");
+
+ return mensaje;
+
+
+}
+
+}
---
+++ src/main/java/com/unitec/localizaamigos/Mensaje.java
@@ -0,0 +1,25 @@
+package com.unitec.localizaamigos;
+
+public class Mensaje {
+    private String titulo;
+    private  String cuerpo;
+
+    public Mensaje() {
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCuerpo() {
+        return cuerpo;
+    }
+
+    public void setCuerpo(String cuerpo) {
+        this.cuerpo = cuerpo;
+    }
+}
